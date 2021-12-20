844ad45cc34578f9b9fcec2658a7e23e1253bbee
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 13:55:45 -0800

    Add files via upload

---
+++ assets/includes/configuration.php
@@ -0,0 +1,16 @@
+<?php
+
+    date_default_timezone_set("America/Los_Angeles");
+
+    try {
+        $database = "website";
+        $host = "198.74.61.19";
+        $username = "littlefd";
+        $password = "StrongPassword1234!";
+        $connection = new PDO("mysql:dbname=$database;host=$host", $username, $password);
+        $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    } catch(PDOException $error) {
+        echo "Connection Failed: " . $error -> getMessage();
+    }
+
+?>
