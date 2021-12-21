d5a03c8dd9dcd195a2abe8140efe67ff4b1c5854
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 18:21:10 -0800

    Add files via upload

--- upload.php
+++ upload.php
@@ -3,8 +3,6 @@
     require_once("includes/classes/Image_Details_Form_Provider.php"); 
     require_once("includes/header.php"); 
 ?>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
@@ -17,23 +15,6 @@
                 $form_provider = new Image_Details_Form_Provider($connection);
                 echo $form_provider -> create_upload_form(); 
             ?>
-        </div>
-
-        <script>
-            $("form").submit(function() {
-                $("#loading_modal").modal("show");
-            });
-        </script>
-
-        <div class="modal fade" id="loading_modal" tabindex="-1" role="dialog" aria-labelledby="loading_modal" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        Please wait. This should only take a moment.
-                        <img src="assets/images/loading-spinner.gif">
-                    </div>
-                </div>
-            </div>
         </div>
 <?php 
     require_once("includes/footer.php"); 
