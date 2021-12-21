61d60462836e7214ba8146f7f482ea0dbb2fe22c
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 19:37:47 -0800

    Add files via upload

--- includes/classes/Image_Processor.php
+++ includes/classes/Image_Processor.php
@@ -12,14 +12,15 @@
 
         public function upload($image_upload_data) {
             $output_file_directory = "assets/images/";
-            $url = $image_upload_data["url"];
+            $url = $image_upload_data -> url;
             $id = uniqid();
             $extension = pathinfo($url, PATHINFO_EXTENSION);
             $path = $output_file_directory . $id . "." . $extension;
             $filename = $id . "." . $extension;
-            $location = $image_upload_data["location"];
-            $description = $image_upload_data["description"];
+            $location = $image_upload_data -> location;
+            $description = $image_upload_data -> description;
             $is_valid_data = $this -> process_data($image_data, $path);
+            echo $url . " " . $id . " " . $extension . " " . $path . " " . $filename . " " . $location . " " . $description;
             if (!$is_valid_data) {
                 return false;
             }
