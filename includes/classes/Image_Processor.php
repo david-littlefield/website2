fa94431a806616bdff4c4ec37b1222200b6872ee
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 22:57:10 -0800

    Add files via upload

--- includes/classes/Image_Processor.php
+++ includes/classes/Image_Processor.php
@@ -27,7 +27,7 @@
                 $filename = $random_id . "." . $content_type;
                 $path = "assets/images/" . $filename;
                 $source_url = $this -> resolve_source_url($headers);
-                if (!download_file($source_url)) {
+                if (!$this -> download_file($source_url)) {
                     echo "Download image failed";
                     return false;
                 }
@@ -39,7 +39,7 @@
             }
             $filename = $random_id . "." . $file_type;
             $path = "assets/images/" . $filename;
-            if (!download_file($url)) {
+            if (!$this -> download_file($url)) {
                 echo "Download image failed";
                 return false;
             }
