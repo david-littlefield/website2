ea917ce3e04e41af272f1369d35dd0b3716175c8
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 18:14:34 -0800

    Add files via upload

--- upload.php
+++ upload.php
@@ -33,6 +33,7 @@
                 </div>
             </div>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <?php 
     require_once("includes/footer.php"); 
 ?>
