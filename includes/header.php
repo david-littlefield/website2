570e95ee58a4050d9438d2b6cad53bdee2bcc025
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 17:34:57 -0800

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -41,10 +41,3 @@
         </div>
     </nav>
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">
-                    <small>Recent Work</small>
-                </h1>
-            </div>
-        </div>
