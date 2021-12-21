0879c101396230e42d587fc51645b011d73563c7
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 17:42:49 -0800

    Add files via upload

--- upload.php
+++ upload.php
@@ -1,32 +1,30 @@
 <?php 
     require_once("includes/header.php"); 
-    require_once("includes/classes/Video_Details_Form_Provider.php"); 
+    require_once("includes/classes/Image_Details_Form_Provider.php"); 
 ?>
+        <div class="column">
+            <?php
+                $form_provider = new Image_Details_Form_Provider($connection);
+                echo $form_provider -> create_upload_form(); 
+            ?>
+        </div>
 
-    <div class="column">
-        <?php
-            $form_provider = new Image_Details_Form_Provider($connection);
-            echo $form_provider -> create_upload_form(); 
-        ?>
-    </div>
+        <script>
+            $("form").submit(function() {
+                $("#loading_modal").modal("show");
+            });
+        </script>
 
-    <script>
-        $("form").submit(function() {
-            $("#loading_modal").modal("show");
-        });
-    </script>
-
-    <div class="modal fade" id="loading_modal" tabindex="-1" role="dialog" aria-labelledby="loading_modal" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-body">
-                    Please wait. This should only take a moment.
-                    <img src="assets/images/icons/loading-spinner.gif">
+        <div class="modal fade" id="loading_modal" tabindex="-1" role="dialog" aria-labelledby="loading_modal" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        Please wait. This should only take a moment.
+                        <img src="assets/images/icons/loading-spinner.gif">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
 <?php 
     require_once("includes/footer.php"); 
 ?>
