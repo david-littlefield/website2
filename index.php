f26f1679169c589901d9d279cf79b7c998c47a7a
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-20 17:37:25 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -46,31 +46,35 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4 footer-blurb-item">
-            <img class="img-circle" loading="lazy" src="http://45.79.166.55/assets/images/7.jpeg" srcset_placeholder="media/100/7.jpeg  1024w, media/100/7.jpeg 640w, media/100/7.jpeg 320w" width="100" height="100">
-            <h3>My Stuff</h3>
-            <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
-                multiply so sixth gathering fifth that man fowl made.</p>
-            <p><a class="btn btn-primary" href="https://unsplash.com/photos/tNCH0sKSZbA">More Stuff</a></p>
-        </div>
-        <div class="col-sm-4 footer-blurb-item">
-            <img class="img-circle" loading="lazy" src="http://45.79.166.55/assets/images/8.jpeg" srcset_placeholder="media/100/8.jpeg  1024w, media/100/8.jpeg 640w, media/100/8.jpeg 320w" width="100" height="100">
-            <h3>Your Stuff</h3>
-            <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
-                multiply so sixth gathering fifth that man fowl made.</p>
-            <p><a class="btn btn-primary" href="https://unsplash.com/photos/vkdOhd_oYic">More Stuff</a></p>
-        </div>
-        <div class="col-sm-4 footer-blurb-item">
-            <img class="img-circle" loading="lazy" src="http://45.79.166.55/assets/images/9.jpeg" srcset_placeholder="media/100/9.jpeg  1024w, media/100/9.jpeg 640w, media/100/9.jpeg 320w" width="100" height="100">
-            <h3>Our Stuff</h3>
-            <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
-                multiply so sixth gathering fifth that man fowl made.</p>
-            <p><a class="btn btn-primary" href="https://unsplash.com/photos/VF4SWxE-MRw">More Stuff</a></p>
+<footer>
+    <div class="footer-blurb">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 footer-blurb-item">
+                    <img class="img-circle" loading="lazy" src="http://45.79.166.55/assets/images/7.jpeg" srcset_placeholder="media/100/7.jpeg  1024w, media/100/7.jpeg 640w, media/100/7.jpeg 320w" width="100" height="100">
+                    <h3>My Stuff</h3>
+                    <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                        multiply so sixth gathering fifth that man fowl made.</p>
+                    <p><a class="btn btn-primary" href="https://unsplash.com/photos/tNCH0sKSZbA">More Stuff</a></p>
+                </div>
+                <div class="col-sm-4 footer-blurb-item">
+                    <img class="img-circle" loading="lazy" src="http://45.79.166.55/assets/images/8.jpeg" srcset_placeholder="media/100/8.jpeg  1024w, media/100/8.jpeg 640w, media/100/8.jpeg 320w" width="100" height="100">
+                    <h3>Your Stuff</h3>
+                    <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                        multiply so sixth gathering fifth that man fowl made.</p>
+                    <p><a class="btn btn-primary" href="https://unsplash.com/photos/vkdOhd_oYic">More Stuff</a></p>
+                </div>
+                <div class="col-sm-4 footer-blurb-item">
+                    <img class="img-circle" loading="lazy" src="http://45.79.166.55/assets/images/9.jpeg" srcset_placeholder="media/100/9.jpeg  1024w, media/100/9.jpeg 640w, media/100/9.jpeg 320w" width="100" height="100">
+                    <h3>Our Stuff</h3>
+                    <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                        multiply so sixth gathering fifth that man fowl made.</p>
+                    <p><a class="btn btn-primary" href="https://unsplash.com/photos/VF4SWxE-MRw">More Stuff</a></p>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+</footer>
 
 <?php 
     require_once("includes/footer.php"); 
