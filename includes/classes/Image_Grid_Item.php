497ed7da1656b5e79f8b1d627a1ba31bdc46d2b9
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-21 19:49:35 -0800

    Add files via upload

--- includes/classes/Image_Grid_Item.php
+++ includes/classes/Image_Grid_Item.php
@@ -9,17 +9,17 @@
         }
 
         public function create() {
-            $unsplash_url = $this -> image -> get_unsplash_url();
+            $id = $this -> image -> get_id();
             $path = $this -> image -> get_path();
             $filename = $this -> image -> get_filename();
             $location = $this -> image -> get_location();
             $description = $this -> image -> get_description();
             return "<div class='col-md-6 portfolio-item'>
-                        <a href='$unsplash_url'>
+                        <a href='/edit.php?id=$id'>
                             <img class='img-responsive' loading='eager' src='$path' srcset_placeholder='media/555/$filename  1024w, media/555/$filename 640w, media/225/$filename  320w' width='555' height='370'>
                         </a>
                         <h3>
-                            <a href='$unsplash_url'>$location</a>
+                            <a href='/edit.php?id=$id'>$location</a>
                         </h3>
                         <p>
                             $description
