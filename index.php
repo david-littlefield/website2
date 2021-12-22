4c3f2d71303edec260f62931cafe5159136a4f1c
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-21 17:36:33 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -17,40 +17,33 @@
                 $image_grid = new Image_Grid($connection);
                 echo $image_grid -> create();
             ?>
-            <div class="row text-center">
-                <div class="col-lg-12">
-                    <ul class="pagination">
-                        
-                        <li>
-                            <a href="#">«</a>
-                        </li>
-                        
-                        <li class="active">
-                            <a href="#">1</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#">2</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#">3</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#">4</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#">5</a>
-                        </li>
+        </div>
 
-                        <li>
-                            <a href="#">»</a>
-                        </li>
-
-                    </ul>
-                </div>
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="#">«</a>
+                    </li>
+                    <li class="active">
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                    <li>
+                        <a href="#">5</a>
+                    </li>
+                    <li>
+                        <a href="#">»</a>
+                    </li>
+                </ul>
             </div>
         </div>
 
