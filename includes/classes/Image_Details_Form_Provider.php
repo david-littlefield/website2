84925140967137816d2d88b71502c5452c48986e
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-21 19:53:33 -0800

    Add files via upload

--- includes/classes/Image_Details_Form_Provider.php
+++ includes/classes/Image_Details_Form_Provider.php
@@ -57,7 +57,7 @@
                 $value = "";
             }
             return "<div class='form-group'>
-                        <textarea class='form-control' placeholder='Description' name='description_input' rows='3' resize='none'>$value</textarea> 
+                        <textarea class='form-control' placeholder='Description' name='description_input' rows='3' style='resize: none'>$value</textarea> 
                     </div>";
         }
 
