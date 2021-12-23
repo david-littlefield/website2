24d0b1fb97b543177a9a1224ac62897454d07929
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-23 12:42:54 -0800

    Add files via upload

---
+++ sample.php
@@ -0,0 +1,38 @@
+<?php
+
+    # attempts to execute specified code
+    try {
+    
+        # specifies information to connect to database
+        $database = "website";
+        $host = "198.74.61.19";
+        $username = "littlefd";
+        $password = "StrongPassword1234!";
+
+        # connects to database
+        $connection = new PDO("mysql:dbname=$database; host=$host", $username, $password);
+        $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    
+    # displays error messages that occur durring attempt
+    } catch(PDOException $error) {
+        echo "Connection Failed: " . $error -> getMessage();
+    }
+
+    class Sample {
+
+        private $connection;
+
+        public function __construct($connection) {
+            $this -> connection = $connection;
+        }
+
+        public function read_records() {
+            $query = $this -> connection -> prepare("SELECT * FROM images");
+            $query -> execute();
+            $records = $query -> fetchall();
+            var_dump($records);
+        }
+
+    }
+
+?>
