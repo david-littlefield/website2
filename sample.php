a77abb22fb5e1188def80455a5f958b31a0ff7ee
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-23 14:42:56 -0800

    Add files via upload

--- sample.php
+++ sample.php
@@ -35,9 +35,6 @@
             $query -> bindParam(":id", $id);
             $query -> execute();
             $this -> record = $query -> fetch(PDO::FETCH_ASSOC);
-            var_dump($this -> record);
-            echo $this -> record;
-            print_r($this -> record);
         }
 
         public function create_record($unsplash_url, $path, $filename, $location, $description) {
@@ -83,8 +80,7 @@
     $location = "Da Nang, Vietnam";
     $description = "The Non Nuoc beach is located at the foot of the Marble Mountains and extends over 5 km. This beach has calm waves and crystal clear blue water all year round. You can also eat locally caught fresh fish at one of the restaurants. It is also an ideal place for sports such as surfing, windsurfing, volleyball, etc.";
     $sample -> create_record($unsplash_url, $path, $filename, $location, $description);
-    $id = $this -> connection -> lastInsertId();
-    echo $id;
+    $id = $connection -> lastInsertId();
     $sample -> read_record($id);
     var_dump($sample -> record);
 
