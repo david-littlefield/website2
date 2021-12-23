8fe9c72d1c36e6827299d05c5dfa7b31408ad729
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-22 18:23:31 -0800

    Add files via upload

---
+++ static.php
@@ -0,0 +1,173 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio</title>
+    <link rel="shortcut icon" href="/">
+    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body class="vsc-initialized">
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">
+                    Portfolio
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="/">Recent</a>
+                    </li>
+                    <li>
+                        <a href="/">Upload</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="/">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    <small>Recent</small>
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="/">
+                    <img class="img-responsive" loading="eager" src="assets/img/SYx3UCHZJlo.jpeg" srcset_placeholder="media/555/SYx3UCHZJlo.jpeg  1024w, media/555/SYx3UCHZJlo.jpeg 640w, media/225/SYx3UCHZJlo.jpeg  320w" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="/">Santa Monica, United States</a>
+                </h3>
+                <p>Located just west of Downtown Los Angeles, Santa Monica beach is an iconic example of the famed Southern California beaches. With few Los Angeles beach options offering both large expanses of beach, bike trails and nearby activities, Santa Monica Beach has exactly what you're looking for.</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="/">
+                    <img class="img-responsive" loading="eager" src="assets/img/o_rKNRoNz8s.jpeg" srcset_placeholder="media/555/o_rKNRoNz8s.jpeg  1024w, media/555/o_rKNRoNz8s.jpeg 640w, media/225/o_rKNRoNz8s.jpeg 320w" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="/">South Coast, Australia</a>
+                </h3>
+                <p>Stretching from Stanwell Park in the north to Bittangabee Bay in the south, the wild and wonderful southern shoreline delivers glittering ocean, dramatic rock formations and some of the whitest sands in the world. The entire length is part of the legendary Sydney to Melbourne Coastal Drive, but starting from Sydney you're on the jaw-dropping Grand Pacific Drive for 140km.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="/">
+                    <img class="img-responsive" loading="eager" src="assets/img/MS7KD9Ti7FQ.jpeg" srcset_placeholder="media/555/MS7KD9Ti7FQ.jpeg  1024w, media/555/MS7KD9Ti7FQ.jpeg 640w, media/225/MS7KD9Ti7FQ.jpeg 320w" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="/">Bali, Indonesia</a>
+                </h3>
+                <p>The beach of Legian stretches 2 km, starting from the northernmost point of Kuta Beach. It's quite challenging to define the borders of these beaches, and they both offer very similar activities like surfing, partying, and shopping.</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="/">
+                    <img class="img-responsive" loading="eager" src="assets/img/1VYdx37vOGQ.jpeg" srcset_placeholder="media/555/1VYdx37vOGQ.jpeg  1024w, media/555/1VYdx37vOGQ.jpeg 640w, media/225/1VYdx37vOGQ.jpeg 320w" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="/">Nova Scotia, Canada</a>
+                </h3>
+                <p>Martinique Beach Provincial Park is the longest sandy beach in Nova Scotia. Visitors can enjoy this 5-km crescent white-sand beach, with its open and wooded picnic areas located behind sand dunes. The beach is supervised during the months of July and August; change houses are available.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 portfolio-item">
+                <a href="/">
+                    <img class="img-responsive" loading="lazy" src="assets/img/7yqyQQXgOT8.jpeg" srcset_placeholder="media/555/7yqyQQXgOT8.jpeg  1024w, media/555/7yqyQQXgOT8.jpeg 640w, media/225/7yqyQQXgOT8.jpeg 320w" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="/">Luzon, Philippines</a>
+                </h3>
+                <p>Saud Beach is more popularly known as "Pagudpud Beach." The picturesque 2-kilometer white sand beach is often featured in travel photos of Ilocos Norte, and is known for its pristine waters and beautiful coast lined with coconut palms.</p>
+            </div>
+            <div class="col-md-6 portfolio-item">
+                <a href="/">
+                    <img class="img-responsive" loading="lazy" src="assets/img/PfK35Iw1JAo.jpeg" srcset_placeholder="media/555/PfK35Iw1JAo.jpeg  1024w, media/555/PfK35Iw1JAo.jpeg 640w, media/225/PfK35Iw1JAo.jpeg 320w" width="555" height="370">
+                </a>
+                <h3>
+                    <a href="/">Cap de Ses Salines, Spain</a>
+                </h3>
+                <p>The Cap de Ses Salines is the southernmost point on Mallorca. When the weather is good, you can see the almost uninhabited island of Cabrera, which neighbours Mallorca to the south. There is also an attractive lighthouse present, which you can reach either by car or bicycle.</p>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    <li>
+                        <a href="/">«</a>
+                    </li>
+                    <li class="active">
+                        <a href="/">1</a>
+                    </li>
+                    <li>
+                        <a href="/">2</a>
+                    </li>
+                    <li>
+                        <a href="/">3</a>
+                    </li>
+                    <li>
+                        <a href="/">4</a>
+                    </li>
+                    <li>
+                        <a href="/">5</a>
+                    </li>
+                    <li>
+                        <a href="/">»</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-blurb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4 footer-blurb-item">
+                        <img class="img-circle" loading="lazy" src="assets/img/tNCH0sKSZbA.jpeg" srcset_placeholder="media/100/tNCH0sKSZbA.jpeg  1024w, media/100/tNCH0sKSZbA.jpeg 640w, media/100/tNCH0sKSZbA.jpeg 320w" width="100" height="100">
+                        <h3>My Stuff</h3>
+                        <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                            multiply so sixth gathering fifth that man fowl made.</p>
+                        <p><a class="btn btn-primary" href="/">More Stuff</a></p>
+                    </div>
+                    <div class="col-sm-4 footer-blurb-item">
+                        <img class="img-circle" loading="lazy" src="assets/img/vkdOhd_oYic.jpeg" srcset_placeholder="media/100/vkdOhd_oYic.jpeg  1024w, media/100/vkdOhd_oYic.jpeg 640w, media/100/vkdOhd_oYic.jpeg 320w" width="100" height="100">
+                        <h3>Your Stuff</h3>
+                        <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                            multiply so sixth gathering fifth that man fowl made.</p>
+                        <p><a class="btn btn-primary" href="/">More Stuff</a></p>
+                    </div>
+                    <div class="col-sm-4 footer-blurb-item">
+                        <img class="img-circle" loading="lazy" src="assets/img/VF4SWxE-MRw.jpeg" srcset_placeholder="media/100/VF4SWxE-MRw.jpeg  1024w, media/100/VF4SWxE-MRw.jpeg 640w, media/100/VF4SWxE-MRw.jpeg 320w" width="100" height="100">
+                        <h3>Our Stuff</h3>
+                        <p>Upon yielding, kind sea subdue very seed sixth them lesser one lesser there earth days were
+                            multiply so sixth gathering fifth that man fowl made.</p>
+                        <p><a class="btn btn-primary" href="/">More Stuff</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <p>Copyright © Example.com 2021</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
