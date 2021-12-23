538ed61d49c7dd3f8ec3450cdcbac90da1af21ab
David Littlefield <30560737+david-littlefield@users.noreply.github.com>
2021-12-23 13:13:54 -0800

    Add files via upload

--- sample.php
+++ sample.php
@@ -35,6 +35,7 @@
             $query -> bindParam(":id", $id);
             $query -> execute();
             $this -> record = $query -> fetch(PDO::FETCH_ASSOC);
+            var_dump($this -> record);
         }
 
         public function create_record($unsplash_url, $path, $filename, $location, $description) {
